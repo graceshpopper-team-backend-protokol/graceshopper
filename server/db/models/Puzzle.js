d36f761b1f721452ad98a90a48368f00a9e38bd8
Katrina <katrossi414@gmail.com>
2023-02-28 10:49:31 -0600

    created Puzzel model

--- server/db/models/Puzzle.js
+++ server/db/models/Puzzle.js
@@ -1,8 +1,30 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Puzzle = db.define(
-  
+const Puzzle = db.define('puzzle', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  imgURL: {
+    type: Sequelize.STRING
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false
+  }
+}
+
 )
 
 module.exports = Puzzle
