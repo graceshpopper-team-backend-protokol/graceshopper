2d2e534ca3b10050cfda9f293a7d122165e4a72b
Sarah Stephens <sarahstephens84@gmail.com>
2023-03-01 09:33:28 -0500

    update helper arrays

--- script/Puzzledata.js
+++ script/Puzzledata.js
@@ -8,12 +8,12 @@ for (let i = 0; i < 50; i ++) {
         name: faker.lorem.word({length: { min: 2, max: 5}}),
         description: faker.lorem.paragraph(),
         stockQuantity: faker.datatype.number({min: 0}),
-        puzzlePieces: faker.helpers.arrayElement(['250 pieces','500 pieces', '1000 pieces']),
-        price: faker.helpers.arrayElement(['$12.99', '$15.99', '$19.99']),
+        puzzlePieces: faker.helpers.arrayElement(['250 pieces','500 pieces', '1000 pieces', 1]),
+        price: faker.helpers.arrayElement(['$12.99', '$15.99', '$19.99'], 1),
           //if puzzlePieces = 250 pieces, return $12.99  
           //if puzzlePieces = 500 pieces, return $15.99 
           //if puzzlePieces = 1000 pieces, return $19.99 
-        imgURL: 'https://picsum.photos/200',
+        // imgURL: 'https://picsum.photos/200',
     }
     puzzles.push(newPuzzle)
 }
@@ -28,7 +28,7 @@ const seedPuzzles = async () => {
             stockQuantity: puzzle.stockQuantity,
             puzzlePieces: puzzle.puzzlePieces,
             price: puzzle.price,
-            image: puzzle.imgURL,
+            // image: puzzle.imgURL,
         })
         ))
     } catch (err) {
