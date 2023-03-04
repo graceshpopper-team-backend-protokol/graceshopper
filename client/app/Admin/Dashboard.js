15838b601a5228397968ccac102856e5f54a2c3f
megtb <megbickerstaff@gmail.com>
2023-03-04 14:48:58 -0500

    Dashboard adding puzzles

--- client/app/Admin/Dashboard.js
+++ client/app/Admin/Dashboard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-import AllPuzzles from '../../features/pages/AllPuzzles';
-
-const Dashboard = () => {
+const Dashboard = ( { puzzle }) => {
     return(
         <div>
-        <h3>Dashboard</h3>
-        <section><AllPuzzles /></section>
+            <h3>Dashboard</h3>
+            <div className="dashboard">
+                <h1>{puzzle.name}</h1>
+                <p>{puzzle.description}</p>
+                <p>{puzzle.puzzlePieces}</p>
+                <p>{puzzle.price}</p>
+                <p>{puzzle.imgUrl.defaultValue}</p>
+                
+
+            </div>
         </div>
     );
 }
