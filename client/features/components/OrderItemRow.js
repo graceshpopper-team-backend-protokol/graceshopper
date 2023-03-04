52079b63a1141da05504627088c9be4e999cd038
sstephens0907 <102310021+sstephens0907@users.noreply.github.com>
2023-03-04 17:41:11 -0500

    fix style module imports

--- client/features/components/OrderItemRow.js
+++ client/features/components/OrderItemRow.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import styles from "../styles/OrderItemRow.js"
+import styles from "../styles/OrderItemRow.module.css"
 
 const OrderItemCard = ({ orderItem }) => {
   const puzzle = orderItem.puzzle;
--- client/features/pages/Cart.js
+++ client/features/pages/Cart.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { createOrderFromOrderItems, fetchOrderItems, selectOrder } from "../store/orderSlice.js";
-import styles from "../styles/Cart.js";
+import styles from "../styles/Cart.module.css";
 import OrderItemRow from "../components/OrderItemRow.js";
 
 //needs to pass down cart(Order) and userId on Nav
