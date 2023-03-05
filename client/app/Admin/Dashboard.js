dd6531af6c29313024cbc6833d0b01c3cbee7977
megtb <megbickerstaff@gmail.com>
2023-03-05 09:05:50 -0500

    Dashboard page displaying puzzles

--- client/app/Admin/Dashboard.js
+++ client/app/Admin/Dashboard.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { fetchPuzzles, selectPuzzles } from '../../features/store/allPuzzlesSlice';
+import PuzzleCard from '../../features/components/PuzzleCard';
 
-const Dashboard = ( { puzzle }) => {
+
+const Dashboard = () => {
+  const dispatch = useDispatch();
+  const puzzles = useSelector(selectPuzzles);
+  
+
+  useEffect(() => {
+    dispatch(fetchPuzzles());
+  }, [dispatch]);
+
+  console.log(`puzzles from dashboard: ${puzzles}`);
+
     return(
         <div>
-            <h3>Dashboard</h3>
+            <h1>Dashboard</h1>
             <div className="dashboard">
-                <h1>{puzzle.name}</h1>
-                <p>{puzzle.description}</p>
-                <p>{puzzle.puzzlePieces}</p>
-                <p>{puzzle.price}</p>
-                <p>{puzzle.imgUrl.defaultValue}</p>
+                {puzzles?.map((puzzle) => {
+                return (
+                    <div>
+                        <p>{puzzle.name}</p>
+                        <p>{puzzle.description}</p>
+                        <p>{puzzle.puzzlePieces}</p>
+                        <p>{puzzle.price}</p>
+                        <p>{puzzle.imgUrl}</p>
+                    </div>
+                  );
+                })}
                 
-
             </div>
         </div>
     );
--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router;
 router.use("/users", require("./users"));
 router.use("/puzzles", require("./puzzles"));
 router.use("/cart", require("./cart"));
+router.use("/dashboard", require("./dashboard"));
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
