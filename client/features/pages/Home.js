c9db081eae987fa16f1f4cf78339a13769cb1a60
sstephens0907 <102310021+sstephens0907@users.noreply.github.com>
2023-03-06 16:04:33 -0500

    homepage updates

--- client/features/pages/Home.js
+++ client/features/pages/Home.js
@@ -1,22 +1,65 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchPuzzles } from "../store/allPuzzlesSlice";
 
-import AllPuzzles from "./AllPuzzles";
+
+function PuzzleDisplay({ puzzles }) {
+  if (!puzzles) {
+    return <div>Loading puzzles...</div>
+  }
+  const selectedPuzzles = useMemo(() => {
+    const randomIndexes = new Set();
+    while (randomIndexes.size < 3 && randomIndexes.size < puzzles.length) {
+      randomIndexes.add(Math.floor(Math.random() * puzzles.length));
+    }
+    return Array.from(randomIndexes).map((index) => puzzles[index]);
+  }, [puzzles]);
+
+  return (
+    <div>
+      {selectedPuzzles.map((puzzle) => (
+        <div key={puzzle.id}>
+          <Link to={`/puzzles/${puzzle.id}`}>
+          <img src={puzzle.imgURL} />
+          <h2>{puzzle.name}</h2>
+          <h3>{puzzle.price}</h3>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 /**
  * COMPONENT
  */
-const Home = (props) => {
+const Home = props => {
   const username = useSelector((state) => state.auth.me.firstName) || "Puzzler";
+  const dispatch = useDispatch();
+  const puzzles = useSelector((state) => state.puzzles);
+
+  useEffect(() => {
+    dispatch(fetchPuzzles())
+  }, [dispatch]);
+
+  
 
   return (
     <div>
       <h3>Welcome, {username}!</h3>
-      <section>
-        <AllPuzzles />
+      <section className="bestsellers">
+       <h1>Best Sellers</h1>
+       <PuzzleDisplay puzzles={puzzles}/>
+      </section>
+      <section className="staffpicks">
+        <h1>Staff Picks</h1>
+        <PuzzleDisplay puzzles={puzzles}/>
       </section>
     </div>
   );
 };
+
+
 
 export default Home;
