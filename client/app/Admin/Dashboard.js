82b0ada923f4948dc3e4357bcee5640bce9b8f54
vik-wed <wedel.viktoria@gmail.com>
2023-03-07 09:34:01 -0500

    Update cart to merge guestcart with usercart on login

--- client/app/Admin/Dashboard.js
+++ client/app/Admin/Dashboard.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchPuzzles, selectPuzzles } from '../../features/store/allPuzzlesSlice';
-import { deletePuzzleAsync } from '../../features/store/allPuzzlesSlice';
-import AddPuzzle from './AddPuzzle';
-
+import {
+  fetchPuzzles,
+  selectPuzzles,
+} from "../../features/store/allPuzzlesSlice";
+import { deletePuzzleAsync } from "../../features/store/allPuzzlesSlice";
+import AddPuzzle from "./AddPuzzle";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
@@ -25,33 +27,48 @@ const Dashboard = () => {
 
       <div className="dashboard">
         <Link to={`/dashboard/users`}>View Users</Link>
-         <AddPuzzle />
-                <h3>Edit Existing Puzzles</h3>
-                <ol>
-                {puzzles.map((puzzle) => {
-                return (
-                    <div>
-                        
-                        <p><strong>Name: </strong>{puzzle.name}</p>
-                        <p><strong>Description: </strong>{puzzle.description}</p>
-                        <p><strong>Number of Pieces: </strong>{puzzle.puzzlePieces}</p>
-                        <p><strong>Price: </strong>{puzzle.price}</p>
-                        <p><strong>Puzzle ID: </strong>{puzzle.id}</p>
-                        
-                        <Link to={`/dashboard/puzzles/${puzzle.id}`}>Edit Puzzle
-                        </Link>
+        <AddPuzzle />
+        <h3>Edit Existing Puzzles</h3>
+        <ol>
+          {puzzles.map((puzzle) => {
+            return (
+              <div>
+                <p>
+                  <strong>Name: </strong>
+                  {puzzle.name}
+                </p>
+                <p>
+                  <strong>Description: </strong>
+                  {puzzle.description}
+                </p>
+                <p>
+                  <strong>Number of Pieces: </strong>
+                  {puzzle.puzzlePieces}
+                </p>
+                <p>
+                  <strong>Price: </strong>
+                  {puzzle.price}
+                </p>
+                <p>
+                  <strong>Puzzle ID: </strong>
+                  {puzzle.id}
+                </p>
 
-                        <button id='delete-button' onClick={() => {
-                            handlePuzzleDelete(puzzle.id);
-                        }}
-                        >
-                        Delete puzzle
-                        </button>
-                    </div>
-                  );
-                })}
-                </ol>
-               <div>
+                <Link to={`/dashboard/puzzles/${puzzle.id}`}>Edit Puzzle</Link>
+
+                <button
+                  id="delete-button"
+                  onClick={() => {
+                    handlePuzzleDelete(puzzle.id);
+                  }}
+                >
+                  Delete puzzle
+                </button>
+              </div>
+            );
+          })}
+        </ol>
+      </div>
     </div>
   );
 };
